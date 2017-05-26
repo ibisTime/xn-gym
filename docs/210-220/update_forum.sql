4d696a7b3f6fdcee5307d68e05937cf09958cf8e
asus <asus@DESKTOP-N61H2QE>
2017-05-26 17:07:11 +0800

    脚本更新

--- docs/210-220/update_forum.sql
+++ docs/210-220/update_forum.sql
@@ -1,15 +1,15 @@
 ALTER TABLE `tforum_post` 
-ADD COLUMN `nickname` VARCHAR(32) NULL COMMENT '昵称' AFTER `publisher`,
+ADD COLUMN `nickname` VARCHAR(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称' AFTER `publisher`,
 ADD COLUMN `photo` TEXT NULL COMMENT '头像' AFTER `nickname`,
 ADD COLUMN `login_name` VARCHAR(32) NULL COMMENT '登陆名' AFTER `photo`;
 
 ALTER TABLE `tforum_comment` 
-ADD COLUMN `nickname` VARCHAR(32) NULL COMMENT '昵称' AFTER `commer`,
+ADD COLUMN `nickname` VARCHAR(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称' AFTER `commer`,
 ADD COLUMN `photo` TEXT NULL COMMENT '头像' AFTER `nickname`,
 ADD COLUMN `login_name` VARCHAR(32) NULL COMMENT '登陆名' AFTER `photo`;
 
 ALTER TABLE `tforum_post_talk` 
-ADD COLUMN `nickname` VARCHAR(32) NULL COMMENT '昵称' AFTER `talker`,
+ADD COLUMN `nickname` VARCHAR(64) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '昵称' AFTER `talker`,
 ADD COLUMN `photo` TEXT NULL COMMENT '头像' AFTER `nickname`,
 ADD COLUMN `login_name` VARCHAR(32) NULL COMMENT '登陆名' AFTER `photo`;
 
